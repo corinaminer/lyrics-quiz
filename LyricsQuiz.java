15888dcd0eb0f57f061fd8afb7d610b1644e646c
Corina Miner <c.miner19@gmail.com>
2020-07-19 00:44:22 -0700

    Clean up old Java code (#1)

--- LyricsQuiz.java
+++ LyricsQuiz.java
@@ -1,12 +1,11 @@
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class Driver {
+public class LyricsQuiz {
 
   public static void main(String[] args) {
-    System.out.println(37 / 6);
-    ArrayList<Song> songs = Util.getSongs("lyrics.txt");
+    List<Song> songs = Util.getSongs("lyrics.txt");
     Random r = new Random();
-    GUI gui = new GUI(songs.get(r.nextInt(songs.size())));
+    new GUI(songs.get(r.nextInt(songs.size())));
   }
 }
